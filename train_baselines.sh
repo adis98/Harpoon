56c38ed64738b1b02631065475a79ad16ea47c28
adis98 <aditya.ssr@gmail.com>
2025-08-27 19:02:33 +0200

    added harpoon ordinal sampling basic manifold

--- train_baselines.sh
+++ train_baselines.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # Define the options for the synth_mask parameter
-options_dataset=("adult" "bean" "california" "default" "gesture" "letter" "magic" "news" "shoppers")
+options_dataset=("adult" "bean" "california" "default" "gesture" "letter" "magic" "shoppers")
 
 
 for dataset in "${options_dataset[@]}"
 do
-  python3.12 train_gain.py --dataname $dataset
+  python3.12 train_harpoon_ordinal.py --dataname $dataset
+#  python3.12 train_gain.py --dataname $dataset
 #  python3.12 train_remasker.py --dataname $dataset
 #  python3.12 train_GReaT.py --dataname $dataset
 #  python3.12 train_repaint.py --dataname $dataset
 #  python3.12 train_diffputer.py --dataname $dataset
 done
-sendemail -f aditya.ssr@gmail.com -t aditya.ssr@gmail.com -u "Training Complete" -s smtp.gmail.com:587 -o tls=yes -xu aditya.ssr@gmail.com -xp zfclbeznrksnrhfs -m "Remasker"
+sendemail -f aditya.ssr@gmail.com -t aditya.ssr@gmail.com -u "Training Complete" -s smtp.gmail.com:587 -o tls=yes -xu aditya.ssr@gmail.com -xp zfclbeznrksnrhfs -m "Harpoon Ordinal"
